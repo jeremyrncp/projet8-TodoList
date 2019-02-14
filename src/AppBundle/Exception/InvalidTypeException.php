7aa7d79fcea73ca0526af6743fd976f0b2c473e8
Jeremy Gaultier <contact@webmezenc.com>
2019-02-14 20:08:57 +0100

    tech(exception): add two exceptions
    invalid type exception
    missing necessary operation exception

---
+++ src/AppBundle/Exception/InvalidTypeException.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Crée par Jérémy Gaultier <contact@webmezenc.com>
+ * Tous droits réservés
+ */
+
+namespace AppBundle\Exception;
+
+
+class InvalidTypeException extends \Exception
+{
+
+}
